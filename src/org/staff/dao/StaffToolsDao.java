9015a8798587150626ee488430fd3e3e79c13330
Sandeep Das <sdsts28@gmail.com>
2022-03-13 06:16:00 -0400

    Added clear create read and  update

--- src/org/staff/dao/StaffToolsDao.java
+++ src/org/staff/dao/StaffToolsDao.java
@@ -3,7 +3,9 @@ package org.staff.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 
 import org.humber.SQLInfo.OracleInfo;
@@ -17,7 +19,7 @@ public class StaffToolsDao {
 		System.out.println("Here");
 		createConnection();
 	}
-	private void createConnection() {
+	public boolean createConnection() {
 		// TODO Auto-generated method stub
 		if(connection!=null)
 		{
@@ -26,6 +28,7 @@ public class StaffToolsDao {
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				return false;
 			}
 			
 			
@@ -36,18 +39,22 @@ public class StaffToolsDao {
 				Class.forName(OracleInfo.DRIVER_CLASS_MYSQL);
 				connection=DriverManager.getConnection(  
 						OracleInfo.URL,OracleInfo.userName,OracleInfo.pwd);  
-				System.out.println("Connection created");;
+				System.out.println("Connection created");
+				return true;
 				
 			} catch (ClassNotFoundException classNotFoundException) {
 				// TODO: handle exception
 				classNotFoundException.printStackTrace();
+				return false;
 			} catch (SQLException sqlException) {
 				// TODO: handle exception
 				sqlException.printStackTrace();
+				return false;
 			}
+			
 		
 	}
-	public void insertRecord(Staff staff) {
+	public void insertRecord(Staff staff) throws SQLIntegrityConstraintViolationException {
 		try {
 			Statement statement = connection.createStatement();
 			String varname1 = ""
@@ -70,9 +77,77 @@ public class StaffToolsDao {
 			System.out.println(rows + " rows inserted");
 			
 			
+			ResultSet resultSet =  statement.executeQuery("select * from staff");
+			while (resultSet.next()) {
+				System.out.println(resultSet.getString(1));
+				
+			}
+			
+		} catch (SQLIntegrityConstraintViolationException e) {
+			// TODO: handle exception
+			throw e;
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public Staff findStaff(String staffId) {
+		// TODO Auto-generated method stub
+		System.out.println(staffId);
+		try {
+			Statement statement = connection.createStatement();
+			String varname1 = "select * from staff where id = '"+staffId+"'";
+			PreparedStatement preparedStatement = connection.prepareStatement(varname1);
+			//preparedStatement.setString(1, staffId);
+			
+			ResultSet resultSet =  preparedStatement.executeQuery();
+			
+			while(resultSet.next()) {
+				
+				//System.out.println(resultSet.getString(1)+" "+resultSet.getString(2));
+				return new Staff(resultSet.getString(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6),resultSet.getString(7), resultSet.getString(8), resultSet.getString(9));
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public void updateRecordById(Staff updateStaff) {
+		// TODO Auto-generated method stub
+		try {
+			Statement statement = connection.createStatement();
+			String varname1 = ""
+					+ "UPDATE staff "
+					+ "SET lastname = ?, firstname= ?, mi = ? , address = ?, city = ?, state = ?, telephone = ? "
+					+ "WHERE id = ?";
+			PreparedStatement preparedStatement = connection.prepareStatement(varname1);
+			preparedStatement.setString(1, updateStaff.getLastName());
+			preparedStatement.setString(2, updateStaff.getLastName());
+			preparedStatement.setString(3, updateStaff.getGender());
+			preparedStatement.setString(4, updateStaff.getAddress());
+			preparedStatement.setString(5, updateStaff.getCity());
+			preparedStatement.setString(6, updateStaff.getState());
+			preparedStatement.setString(7, updateStaff.getTelephone());
+			preparedStatement.setString(8, updateStaff.getId());
+			
+			int rows = 0;
+			rows = preparedStatement.executeUpdate();
+			System.out.println(rows + " rows updated");
+			
+			
+			ResultSet resultSet =  statement.executeQuery("select * from staff");
+			while (resultSet.next()) {
+				System.out.println(resultSet.getString(1));
+				
+			}
+			
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
+		
 	}
 }
--- src/org/staff/tools/StaffingApplicationFrame.java
+++ src/org/staff/tools/StaffingApplicationFrame.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -66,10 +67,61 @@ public class StaffingApplicationFrame extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			if (e.getSource()==insertButton) {
+			if (e.getSource() == insertButton) {
 				
 				Staff insertStaff = new Staff(IDTextField.getText(), lastNameTextField.getText(), firstNameTextField.getText(), sexTextField.getText(), addressTextField.getText(), cityTextField.getText(), stateTextField.getText(), telephoneTextField.getText(), IDTextField.getText()+"@humber.ca");
-				staffToolsDao.insertRecord(insertStaff);
+				try {
+					staffToolsDao.insertRecord(insertStaff);
+					connectivityLabel.setText("1 row updated");
+				} catch (SQLIntegrityConstraintViolationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					connectivityLabel.setText("Duplicate primary key!");
+				}
+				
+			}
+			
+			if(e.getSource() == viewButton) {
+				Staff getStaff;
+				
+				getStaff = staffToolsDao.findStaff(IDTextField.getText());
+				if(getStaff!=null) {
+				lastNameTextField.setText(getStaff.getLastName());
+				firstNameTextField.setText(getStaff.getFirstName());
+				sexTextField.setText(getStaff.getGender());
+				telephoneTextField.setText(getStaff.getTelephone());
+				cityTextField.setText(getStaff.getCity());
+				addressTextField.setText(getStaff.getAddress());
+				stateTextField.setText(getStaff.getState());
+				connectivityLabel.setText("Staff ID: "+IDTextField.getText()+" found");
+				}
+				else
+					connectivityLabel.setText("No record found");
+			}
+			
+			if(e.getSource() == updateButton) {
+				Staff updateStaff = new Staff();
+				updateStaff = staffToolsDao.findStaff(IDTextField.getText());
+				updateStaff.setFirstName(firstNameTextField.getText());
+				updateStaff.setLastName(lastNameTextField.getText());
+				updateStaff.setGender(sexTextField.getText());
+				updateStaff.setCity(cityTextField.getText());
+				updateStaff.setAddress(addressTextField.getText());
+				staffToolsDao.updateRecordById(updateStaff); 
+				connectivityLabel.setText("1 row updated");
+			}
+			
+			if(e.getSource() == clearButton) {
+				firstNameTextField.setText("");
+				lastNameTextField.setText("");
+				IDTextField.setText("");
+				cityTextField.setText("");
+				sexTextField.setText("");
+				telephoneTextField.setText("");
+				stateTextField.setText("");
+				if (staffToolsDao.createConnection()==true) {
+					connectivityLabel.setText("DB Connected");
+				}
 			}
 
 		}
@@ -82,34 +134,23 @@ public class StaffingApplicationFrame extends JFrame {
 		createStaffInformationPanel();
 		createOperationsPanel();
 		bundleLayout();
-		connectDB();
-	}
-
-	private void connectDB() {
-		// TODO Auto-generated method stub
-		Connection con = null;
-		try {
-			Class.forName(OracleInfo.DRIVER_CLASS_MYSQL);
-			con=DriverManager.getConnection(  
-					OracleInfo.URL,OracleInfo.userName,OracleInfo.pwd);  
+		if (staffToolsDao.createConnection()==true) {
 			connectivityLabel.setText("DB Connected");
-		} catch (ClassNotFoundException classNotFoundException) {
-			// TODO: handle exception
-			classNotFoundException.printStackTrace();
-		} catch (SQLException sqlException) {
-			// TODO: handle exception
-			sqlException.printStackTrace();
-		}finally {
-			try {
-				if (con != null)
-					con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 	}
 
+	/*
+	 * private void connectDB() { // TODO Auto-generated method stub Connection con
+	 * = null; try { Class.forName(OracleInfo.DRIVER_CLASS_MYSQL);
+	 * con=DriverManager.getConnection(
+	 * OracleInfo.URL,OracleInfo.userName,OracleInfo.pwd);
+	 * 
+	 * } catch (ClassNotFoundException classNotFoundException) { // TODO: handle
+	 * exception classNotFoundException.printStackTrace(); } catch (SQLException
+	 * sqlException) { // TODO: handle exception sqlException.printStackTrace();
+	 * }finally { try { if (con != null) con.close(); } catch (SQLException e) { //
+	 * TODO Auto-generated catch block e.printStackTrace(); } } }
+	 */
 	private void bundleLayout() {
 		// TODO Auto-generated method stub
 		setSize(500,300);
@@ -128,10 +169,17 @@ public class StaffingApplicationFrame extends JFrame {
 		JPanel wrapperPanel = new JPanel();
 		
 		viewButton = new JButton("View");
+		viewButton.addActionListener(listener);
+		
 		insertButton = new JButton("Insert");
 		insertButton.addActionListener(listener);
+		
 		updateButton = new JButton("Update");
+		updateButton.addActionListener(listener);
+		
 		clearButton = new JButton("Clear");
+		clearButton.addActionListener(listener);
+		
 		connectivityLabel = new JLabel("");
 		
 		wrapperPanel.add(viewButton);
